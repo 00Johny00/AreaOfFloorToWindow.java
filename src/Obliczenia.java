8f3506cef0e9017c58712b421b78c5cc0e331769
00Johny00 <90790986+00Johny00@users.noreply.github.com>
2021-09-18 17:48:46 +0200

    Update Obliczenia.java

--- src/Obliczenia.java
+++ src/Obliczenia.java
@@ -4,8 +4,10 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
+import java.math.BigDecimal;
 import java.math.RoundingMode;
 import java.text.DecimalFormat;
+import java.util.ArrayList;
 
 public class Obliczenia  extends JFrame{
     private JTabbedPane tabbedPane1;
@@ -20,7 +22,8 @@ public class Obliczenia  extends JFrame{
     private JButton dodajOkno;
     private JLabel field;
     private JButton usuńOknoButton;
-    private JLabel stworzoneOkna;
+    private JTextArea textArea1;
+
     Obliczenia() {
         this.setVisible(true);
         this.getContentPane().add(mainPanel);
@@ -44,43 +47,61 @@ public class Obliczenia  extends JFrame{
             public void actionPerformed(ActionEvent e) {
                 System.out.println(textField1.getText()); //Sprawdzamy teksty na konsoli
                 System.out.println(textField3.getText());
-                BigDecimal a = new BigDecimal (Double.parseDouble(textField1.getText())); //Konwesrja Stringów na Double
-                BigDecimal b = new BigDecimal (Double.parseDouble(textField3.getText())); //Konwesrja Stringów na Double
-                BigDecimal powierzchniaPodlogi = a.mulitply(b).multiply(8);   //a*b*8; wysokosc okna * szerokosc * wartość normowana
-                wynik.setText(okrajanieZer.format(powierzchniaPodlogi) + "m2");  // ustawiamy wynik taki jak wyniczek // Sprawdzić czy po zmianie na Big formatujez zer nie potrzebnych tak samo
+                BigDecimal a = new BigDecimal (Double.parseDouble(textField1.getText()));
+                BigDecimal b = new BigDecimal (Double.parseDouble(textField3.getText()));
+                BigDecimal FLOOR_TO_WINDOW_PARAMETER = new BigDecimal(8);
+                BigDecimal powierzchniaPodlogi = a.multiply(b).multiply(FLOOR_TO_WINDOW_PARAMETER);  // wysokosc okna * szerokosc * wartość normowana
+                wynik.setText(okrajanieZer.format(powierzchniaPodlogi) + "m2");  // ustawiamy wynik taki jak wyniczek
                 System.out.println("WYNIK : " + wynik.getText()); // Wyrzucamy sobie wynik na konsole do wglądu
 
             }
 
         });
 
-        double[] oknoWysokosc = new double[10]; // Zrobić ArrayList  : List<String> oknoWysokosc = new ArrayList<>(100);
-        double[] oknoSzerokosc = new double[10]; //List<String> oknoSzerokosc = new ArrayList<>(100);
-        dodajOkno.addKeyListener(new KeyAdapter() {
-        });
-        dodajOkno.addActionListener(new ActionListener() {
-           int i=1;
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                oknoSzerokosc[i] = Double.parseDouble(textField1.getText()); //Tutaj teraz nie potrzebne dodawanie do indeksu tylko .add(oknoWysokosc(textField1.getText());
-                System.out.println(oknoSzerokosc[i]);
-                oknoWysokosc [i] = Double.parseDouble(textField3.getText());
-                System.out.println(oknoWysokosc[i]);
-                dodajOknoDoListy(oknoSzerokosc[i],oknoWysokosc[i]);
-                 i++;
-                    System.out.println(stworzoneOkna.getText());
-            }
+        ArrayList<String> oknoDodane = new ArrayList<>(20);
 
-            public void dodajOknoDoListy(double szer, double wys)
-            {
-                JLabel noweOkno = new JLabel("Szerokość: " + oknoSzerokosc[i] + "m2" + "Wysokość: " + oknoWysokosc[i] + "m2");
-                mainPanel.add(noweOkno);
+       // double[] oknoWysokosc = new double[10];
+      //  double[] oknoSzerokosc = new double[10];
+     //   dodajOkno.addKeyListener(new KeyAdapter() {
+      //                           });
+       dodajOkno.addActionListener(new
 
-            }
+            ActionListener() {
+                int i = 1;
+                @Override
+                public void actionPerformed (ActionEvent e){
+                    oknoDodane.add(new String(textField1.getText()));
+                    oknoDodane.add(new String(textField3.getText()));
+                    JOptionPane.showMessageDialog(null,"Dodałeś okno");
+                    System.out.println(oknoDodane);
+                    System.out.println(oknoDodane.get(i));
+               //     stworzoneOkna.setText(String.valueOf(oknoDodane));
+                  wyświetlanieOkien(oknoDodane,i);
+                    i++;
+                 //   stworzoneOkna.setText(stworzoneOkna.getText() +
+                //            "Okno nr " + (i-1)+ " Szerokość:" + (oknoDodane.get(i-1))  + "Wysokość :"+ (oknoDodane.get(i)) + System.lineSeparator()
 
-        });
+                //    );
 
+                }
+            });
+
+        }
+
+    public void wyświetlanieOkien (ArrayList okienko,int i2)
+    {
+        if(i2>1) {
+            textArea1.setText(textArea1.getText() + System.lineSeparator() +
+                    "Okno nr " + (i2) + " Szerokość: " + (okienko.get((i2*2)-2)) + "m2" +"  Wysokość : " + (okienko.get((i2*2)-1)) + "m2"
+            );
+        } else {
+            textArea1.setText(
+                    "Okno nr " + (i2) + " Szerokość: " + (okienko.get(i2 - 1)) + "m2"+ "  Wysokość : " + (okienko.get(i2)) + "m2"
+            );
+        }
     }
+
+
     DecimalFormat okrajanieZer = new DecimalFormat("0.0#"); //Do zaokrąglenia do 2 znaczących
 
     public static void main(String[] args) {
